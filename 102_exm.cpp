f212c60f9db151c734f04ac2ce573ffd384411ef
Shivam Tripathi <37960583+ShivamTripathi21@users.noreply.github.com>
2018-09-29 20:30:49 +0530

    Update 102_exm.cpp

--- 102_exm.cpp
+++ 102_exm.cpp
@@ -8,13 +8,15 @@ int main(){
 
 	cout<<"Enter the larger number: "; cin>>m;
 	cout<<"Enter the smaller number: "; cin>>n;
-
+        cout<<"we always take larger one first"<<endl;
+	int l;
 	while(m%n != 0){
 		int reminder = m%n;
 		m = n;
 		n = reminder;
+		l = 2;
 	} 
 	cout<<"GCD is: "<<n<<endl;
-
+	cout<<"l is: "<<l<<endl;
 	return 0;
 }
